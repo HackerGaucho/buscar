f0cd578b9d6bfc3a8d3278c253ba5548bda4c120
Bell <79753816+phpbell@users.noreply.github.com>
2021-05-27 09:21:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Buscar
+Formulário de busca em diversos sites
+
+## Sites
+1. 4shared
+1. acronimos
+1. alexa
+1. amazon brasil
+1. antônimos
+1. artigos científicos (researchgate)
+1. atom
+1. books
+1. britannica
+1. buscape
+1. cdnjs
+1. dailymotion
+1. discogs
+1. docs (em espanhol)
+1. emojipedia
+1. extensão
+1. extensão (arquivos)
+1. farmácia (são joão)
+1. fricke
+1. genius
+1. github
+1. globo play
+1. icon
+1. imdb
+1. infopedia (dicionario)
+1. informal
+1. last.fm
+1. letras
+1. maps (google)
+1. mdn
+1. mercado livre
+1. music
+1. npm
+1. oembed
+1. packagist
+1. php
+1. quanto custa
+1. reclameaqui
+1. scihub
+1. significados
+1. sinônimos
+1. so
+1. tld
+1. tpb
+1. twitter
+1. username
+1. whosampled
+1. wikipedia
---
+++ redirect.php
@@ -0,0 +1,53 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$buscas=require 'buscas.php';
+$q=@$_GET['q'];
+$q=trim($q);
+$q=urlencode($q);
+$key=@$_GET['key'];
+$removerDiacriticos=function($str){
+    $table = array(//https://stackoverflow.com/a/3635581
+    ' '=>'-', 'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj', 'Ž'=>'Z', 'ž'=>'z', 'C'=>'C', 'c'=>'c', 'C'=>'C', 'c'=>'c',
+    'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+    'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+    'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+    'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+    'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+    'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+    'ÿ'=>'y', 'R'=>'R', 'r'=>'r', "'"=>'-', '"'=>'-'
+    );
+    //return strtr($str, $table);
+    //https://stackoverflow.com/a/18669045
+    return preg_replace('/&([a-z]{1,2})(acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml|caron);/i','$1',htmlentities($str));
+};
+
+if(isset($buscas[$key])){
+    //https://www.php.net/manual/pt_BR/function.str-replace.php
+    $str=$buscas[$key];
+    if($key=='mercado livre'){
+        $q=str_replace('+','-',$q);
+        $q=urldecode($q);
+        $q=$removerDiacriticos($q);
+        //die($q);
+    }
+    if(
+    $key=='dailymotion' OR
+    $key=='tpb'
+    ){
+        $q=urldecode($q);
+    }
+    if(
+    $key=='informal'
+    ){
+        $q=urldecode($q);
+        $q=str_replace(" ","-",$q);
+        $q=urlencode($q);
+    }    
+    $str=str_replace("%s",$q,$str);
+    header('Location: '.$str);
+}else{
+    print '404';
+}
+?>
